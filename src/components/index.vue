db0d2cbe1298c6afffb14e90a9baa49f36561af8
carl <18556906294@163.com>
2021-03-03 19:57:43 +0800

    添加md5

--- src/components/index.vue
+++ src/components/index.vue
@@ -9,15 +9,19 @@
     <a-tab-pane key="3" tab="Base64加密/解密">
       Content of Tab Pane 3
     </a-tab-pane>
+    <a-tab-pane key="4" tab="MD5加密">
+      <md5></md5>
+    </a-tab-pane>
   </a-tabs>
 </template>
 
 <script>
 import uuid from './uuid';
+import md5 from './md5';
 
 export default {
   name: 'index',
-  components: { uuid },
+  components: { uuid, md5 },
   data() {
     return {
       tabIndex: '1'
---
+++ src/components/md5.vue
@@ -0,0 +1,49 @@
+<template>
+  <a-form-model ref="form" :model="data" :rules="rules" :label-col="labelCol" :wrapper-col="wrapperCol">
+    <a-form-model-item label="加密内容" prop="orginValue">
+      <a-input v-model="data.orginValue" />
+    </a-form-model-item>
+    <a-form-model-item label="加密后结果">
+      <a-textarea v-model="data.value" :rows="5" />
+    </a-form-model-item>
+    <a-form-model-item :wrapper-col="{ span: 14, offset: 4 }">
+      <a-button type="primary" @click="generate">
+        生成
+      </a-button>
+      <a-button style="margin-left: 10px;" @click="reset">
+        清空
+      </a-button>
+    </a-form-model-item>
+  </a-form-model>
+</template>
+
+<script>
+export default {
+  name: 'uuid',
+  data() {
+    return {
+      labelCol: { span: 4 },
+      wrapperCol: { span: 14 },
+      data: {
+        orginValue: 1,
+        value: ''
+      },
+      rules: {
+        orginValue: [{ required: true, message: '请输入您需要加密的内容', trigger: 'blur' }]
+      }
+    };
+  },
+  methods: {
+    reset() {
+      this.data.value = '';
+    },
+    generate() {
+      this.$refs.form.validate(resp => {
+        if (resp) {
+          this.data.value = sp.md5Encrypt(this.data.orginValue);
+        }
+      });
+    }
+  }
+};
+</script>
