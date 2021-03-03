ccd8eb0f74ec844a3c5980ebade8f2bd5368e788
Carl Du <carldu@rektec.com.cn>
2021-03-03 16:57:10 +0800

    init

---
+++ src/components/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <a-tabs default-active-key="1" @change="changeTab">
+    <a-tab-pane key="1" tab="uuid生成">
+      <uuid></uuid>
+    </a-tab-pane>
+    <a-tab-pane key="2" tab="AES加密/解密" force-render>
+      Content of Tab Pane 2
+    </a-tab-pane>
+    <a-tab-pane key="3" tab="Base64加密/解密">
+      Content of Tab Pane 3
+    </a-tab-pane>
+  </a-tabs>
+</template>
+
+<script>
+import uuid from './uuid';
+
+export default {
+  name: 'index',
+  components: { uuid },
+  data() {
+    return {
+      tabIndex: '1'
+    };
+  },
+  methods: {
+    changeTab(value) {
+      this.tabIndex = value;
+    }
+  }
+};
+</script>
+
+<style></style>
---
+++ src/components/uuid.vue
@@ -0,0 +1,48 @@
+<template>
+  <a-form-model ref="form" :model="data" :rules="rules" layout="horizontal">
+    <a-form-model-item label="生成数量" prop="count">
+      <a-input-number v-model="data.count" :min="1" :max="10" />
+    </a-form-model-item>
+    <a-form-model-item label="生成的uuid">
+      <a-input v-model="data.value" type="textarea" />
+    </a-form-model-item>
+    <a-form-model-item :wrapper-col="{ span: 14, offset: 4 }">
+      <a-button type="primary" @click="generate">
+        生成
+      </a-button>
+      <a-button style="margin-left: 10px;" @click="reset">
+        清空
+      </a-button>
+    </a-form-model-item>
+  </a-form-model>
+</template>
+
+<script>
+export default {
+  name: 'uuid',
+  data() {
+    return {
+      data: {
+        count: 1,
+        value: ''
+      },
+      rules: {
+        count: [
+          { required: true, message: '请输入生成数量', trigger: 'blur' },
+          { min: 1, max: 10, message: '生成数量大于1小于10', trigger: 'blur' }
+        ]
+      }
+    };
+  },
+  methods: {
+    reset() {
+      this.data.value = '';
+    },
+    generate() {
+      this.data.value = sp.newUUID();
+    }
+  }
+};
+</script>
+
+<style></style>
