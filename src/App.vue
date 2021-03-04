8346b08bf9cbd8af5ec69700bcf1d0bc755b432f
Carl Du <carldu@rektec.com.cn>
2021-03-04 17:46:55 +0800

    添加菜单

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  padding-top: 60px;
 }
 
 body {
--- src/components/index.vue
+++ src/components/index.vue
@@ -1,18 +1,35 @@
 <template>
-  <a-tabs default-active-key="1" @change="changeTab" class="index">
-    <a-tab-pane key="1" tab="uuid生成">
-      <uuid></uuid>
-    </a-tab-pane>
-    <a-tab-pane key="2" tab="AES加密/解密">
-      <aes></aes>
-    </a-tab-pane>
-    <a-tab-pane key="3" tab="Base64加密/解密">
-      <base64></base64>
-    </a-tab-pane>
-    <a-tab-pane key="4" tab="MD5加密">
-      <md5></md5>
-    </a-tab-pane>
-  </a-tabs>
+  <div>
+    <a-menu mode="horizontal" style="text-align: center" theme="dark" @click="menuClick">
+      <a-sub-menu key="encrypt">
+        <span slot="title"><span>编码/加密</span></span>
+        <a-menu-item key="1">
+          MD5 加密
+        </a-menu-item>
+        <a-menu-item key="2">
+          AES 加密/解密
+        </a-menu-item>
+        <a-menu-item key="3">
+          BASE64 编码/解码
+        </a-menu-item>
+        <a-menu-item key="json" disabled>JSON 工具 </a-menu-item>
+      </a-sub-menu>
+    </a-menu>
+    <a-tabs default-active-key="1" @change="changeTab" class="index">
+      <a-tab-pane key="1" tab="uuid生成">
+        <uuid></uuid>
+      </a-tab-pane>
+      <a-tab-pane key="2" tab="AES加密/解密">
+        <aes></aes>
+      </a-tab-pane>
+      <a-tab-pane key="3" tab="Base64加密/解密">
+        <base64></base64>
+      </a-tab-pane>
+      <a-tab-pane key="4" tab="MD5加密">
+        <md5></md5>
+      </a-tab-pane>
+    </a-tabs>
+  </div>
 </template>
 
 <script>
@@ -26,12 +43,16 @@ export default {
   components: { uuid, md5, base64, aes },
   data() {
     return {
-      tabIndex: '1'
+      tabIndex: '1',
+      menus: ['encrypt', 'json']
     };
   },
   methods: {
     changeTab(value) {
       this.tabIndex = value;
+    },
+    menuClick(item) {
+      console.log(item);
     }
   }
 };
